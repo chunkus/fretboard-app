0956cd5df0ceddcedd400d98e86172d106382d7c
chunkus <tobiadebari@gmail.com>
2021-11-26 17:10:55 +0000

    added individual note buttons with sound functionality

--- app/src/main/java/com/example/fretboard/MainActivity.kt
+++ app/src/main/java/com/example/fretboard/MainActivity.kt
@@ -4,8 +4,8 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.GridLayout
 import android.widget.Toast
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,38 +15,28 @@ class MainActivity : AppCompatActivity() {
         val buttonList = mutableListOf<String>()
         val resourceList = mutableListOf<Int>()
 
-        for (i in 1 until 3){
+//        val g : GridLayout = findViewById(R.id.GridLayout1)
+//
+//        for (j in 4 until 78){
+//            val newNoteButton: Button = Button(this)
+//            newNoteButton.text="button$j"
+//            g.addView(newNoteButton)
+//            println("button $j made")
+//        }
+
+        for (i in 1 until 72){
             val buttonID: String = "button$i"
             buttonList.add(buttonID)
 
             val resourceID: Int = this.resources.getIdentifier(buttonList[i-1], "id", this.packageName)
             resourceList.add(resourceID)
+            println("resourceID pointer $i made")
 
             val noteButton: Button = findViewById(resourceList[i-1])
+            println("resourceID pointer $i made")
             setListen(noteButton, i)
         }
-//        for (j in 1 until 3){
-//            val noteButton: Button = findViewById(resourceList[0])
-//            setListen(noteButton, j)
-//        }
 
-
-
-//        noteButton.setOnClickListener {
-//            val toast = Toast.makeText(this, "You played the C note", Toast.LENGTH_SHORT)
-//            toast.show()
-//            val cSound : MediaPlayer = MediaPlayer.create(this, R.raw.soundbite2)
-//            cSound.start()
-//        }
-
-//        val superButton: Button = findViewById(R.id.button2)
-//        superButton.setOnClickListener {
-//            val toast = Toast.makeText(this, "You played the super note", Toast.LENGTH_SHORT)
-//            toast.show()
-//            val sSound : MediaPlayer = MediaPlayer.create(this, R.raw.soundbite3)
-//            sSound.start()
-//
-//        }
     }
     fun setListen(nButton: Button, n: Int){
         nButton.setOnClickListener {
